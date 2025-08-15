ca426d3314cf5b071db1436dd76e494bd0d8f5b4
Arif Faisal <ariffaisal18.19@gmail.com>
2025-08-15 15:09:02 +0600

    Create Leetcode-137-Single_number_2.js

---
+++ 17. Pattern: Bitwise XOR/Leetcode-137-Single_number_2.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let ans =0 ;
+    for (let bitIdx = 0; bitIdx < 32; bitIdx++) {
+        let cnt = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] & (1 << bitIdx)) 
+                cnt++;
+        }
+        if(cnt%3 !== 0)
+            ans = ans|(1<<bitIdx);
+    }
+    return ans;
+};
